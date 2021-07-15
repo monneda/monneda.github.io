0a1a267246e929b699ad0e2bb1c3684acd767c61
ramnasidharta <ramna.thors@gmail.com>
2021-07-15 03:08:04 +0000

    Deploying to gh-pages from @ monneda/monneda.github.io@464a77f8a92c945c3c7aff1ca69905eb8550bc7b 🚀

---
+++ precache-manifest.0b9bbb68ece7f21f556738eddbc31b9e.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c78dff18c1531602784b",
+    "url": "/css/app.9fc7478f.css"
+  },
+  {
+    "revision": "06570be252786270392e",
+    "url": "/css/chunk-vendors.af3221a8.css"
+  },
+  {
+    "revision": "9cea2f98108b16ed09dec91dc0e5a4d4",
+    "url": "/img/bg-intro-desktop.9cea2f98.svg"
+  },
+  {
+    "revision": "da0fb76609225c4afc92d7527e42db11",
+    "url": "/img/bg-intro-mobile.da0fb766.svg"
+  },
+  {
+    "revision": "79e698e3f553b89d024f239001979903",
+    "url": "/img/landing-mockup.79e698e3.png"
+  },
+  {
+    "revision": "3884dec1a18d6305e8f1b67c787c9f5e",
+    "url": "/img/splash/ipad_splash.png"
+  },
+  {
+    "revision": "9b77e1c771212b4f20a306c49ac7f0d6",
+    "url": "/img/splash/ipadpro1_splash.png"
+  },
+  {
+    "revision": "277afea471e2dfcea876c27cf9f41cb5",
+    "url": "/img/splash/ipadpro2_splash.png"
+  },
+  {
+    "revision": "4b4290688bc840b79a319fa3e5f67c86",
+    "url": "/img/splash/ipadpro3_splash.png"
+  },
+  {
+    "revision": "5e212c4406692d969b495c1947075ce5",
+    "url": "/img/splash/iphone5_splash.png"
+  },
+  {
+    "revision": "1e1522b73cb42c3f8cfc4561a8dea034",
+    "url": "/img/splash/iphone6_splash.png"
+  },
+  {
+    "revision": "8caddac11c01496fee169fd16ce42c79",
+    "url": "/img/splash/iphoneplus_splash.png"
+  },
+  {
+    "revision": "e0fc6150653b9e1ec910783631dcdcf3",
+    "url": "/img/splash/iphonex_splash.png"
+  },
+  {
+    "revision": "6b1cd573a874ea6787149f4afc646138",
+    "url": "/img/splash/iphonexr_splash.png"
+  },
+  {
+    "revision": "7080cd01ae3ea4f0080625f35dde5b8b",
+    "url": "/img/splash/iphonexsmax_splash.png"
+  },
+  {
+    "revision": "7fa826ec133fb98b1fc19a5c393b31f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c78dff18c1531602784b",
+    "url": "/js/app.53f48c5a.js"
+  },
+  {
+    "revision": "06570be252786270392e",
+    "url": "/js/chunk-vendors.8f5167ac.js"
+  },
+  {
+    "revision": "7635f91f9572618258b580684bcfb66b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
